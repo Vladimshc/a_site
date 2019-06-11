a1ebccb617b383a75e94c264ca3cd988d4bd1dc4
vladimshc <vladimshc@gmail.com>
2019-06-11 12:14:25 +0300

    add cotial icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -502,6 +502,9 @@ function setNavColor(colorWhite, colorDark) {
         $('.hamburger-inner').css('background-color', color);
         $('.hamburger-inner').cssAfter('background-color', color);
         $('.hamburger-inner').cssBefore('background-color', color);
+
+        // social
+        $('.social-item').css('color', color);
     }
 
     var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
@@ -516,7 +519,12 @@ function setNavColor(colorWhite, colorDark) {
     }
 
     var activColor = rgb2hex($('.swiper-button-white').css('color'));
-    if (homeSlider.activeIndex === 4 || homeSlider.activeIndex === 2) {
+    if (homeSlider.activeIndex === 2 ||
+        homeSlider.activeIndex === 3 ||
+        homeSlider.activeIndex === 4 ||
+        homeSlider.activeIndex === 6 ||
+        homeSlider.activeIndex === 8 ||
+        homeSlider.activeIndex === 10) {
         setActivColor(colorDark)
     } else if (activColor !== colorWhite) {
         setActivColor(colorWhite)
