4da8596a691a29bc562e0d767cc4e7503dde0d74
vladimshc <vladimshc@gmail.com>
2019-06-18 16:04:05 +0300

    contact form php

--- contact_form.php
+++ contact_form.php
@@ -1,22 +1,43 @@
 <?php
 
-if(isset($_POST['submit'])){
-$to = "vladimshc@gmail.com";; // Здесь нужно написать e-mail, куда будут приходить письма
-$from = $_POST['email']; // this is the sender's Email address
-$first_name = $_POST['name'];
-$subject = "Форма отправки сообщений с сайта";
-$subject2 = "Copy of your form submission";
-$message = $first_name . " оставил сообщение:" . "\r\n" . $from "\r\n" . $_POST['message'];
-$message2 = "Here is a copy of your message " . $first_name . "\r\n" . $_POST['message'];
+if($_POST) {
+    $visitor_name = "visitor_name";
+    $visitor_email = "visitor_email";
+    $visitor_message = "visitor_message";
 
-$headers = "From:" . $from;
-$headers2 = "From:" . $to;
+    $subject = "sinsworld.com contact form";
 
-mail($to,$subject,$message,$headers);
-// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='https://google.com.ua'>Вернуться на сайт.</a>";
+    $recipient = "vladimshc@gmail.com";
 
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+
+    if(isset($_POST['message'])) {
+        $visitor_message =  htmlspecialchars($_POST['message']);
+        $send_msg = "Visitor name: " . $visitor_name  . " <br/> " .
+                    "Email: " . $visitor_email  . " <br/> " .
+                    "Message: " . $visitor_message;
+    }
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+
+    if(mail($recipient, $subject, $send_msg, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within soon. </p> Go to home page <a href=\"http://sinsworld.com\">sinsworld.com</a> ";
+        header("Location:http://sinsworld.com");
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+
+} else {
+    echo '<p>Something went wrong</p>';
 }
 
 ?>
