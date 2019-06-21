238011f1121efad66c072d449e48bcf5c2b116e7
vladimshc <vladimshc@gmail.com>
2019-06-21 14:01:34 +0300

    optimase js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 (function (window) {
-
     'use strict';
-
     $.exists = function (selector) {
         return ($(selector).length > 0);
     };
@@ -11,20 +9,13 @@
             PageTransition();
         }
     };
-
     // All Funtions
     PageTransition();
     Menu();
     ms_home_slider();
-    // Sort();
-    // UniteGallery();
-    // ValidForm();
     slider_slide_3();
-    // slider_slide_6_tel();
-    // slider_slide_9_tel();
     setLine();
     setActiv();
-
 })(window);
 
 /*--------------------
@@ -186,113 +177,6 @@ function ms_home_slider() {
     }
 }
 
-/*------------------
- Sort
--------------------*/
-function Sort() {
-    if ($.exists('.filtr-container')) {
-        $('.filtr-container').filterizr();
-        $('.filtr-btn li').on('click', function () {
-            $('.filtr-btn li').removeClass('active');
-            $(this).addClass('active');
-        });
-    }
-}
-
-/*------------------
- Unite-Gallery
--------------------*/
-function UniteGallery() {
-    if ($.exists('#gallery')) {
-        $('#gallery').unitegallery({
-            gallery_theme: 'tiles',
-            tiles_type: "justified",
-            tiles_col_width: 400,
-            tiles_justified_row_height: 400,
-            tiles_justified_space_between: 30,
-            // tile_overlay_color: "#000",
-            tile_overlay_opacity: 0.7,
-            tile_enable_icons: false,
-            tile_textpanel_position: "inside_bottom",
-        });
-    }
-}
-
-/*------------------
- Form Validation
--------------------*/
-function ValidForm() {
-    if ($.exists('#validForm')) {
-        $('.form-control').focus(function () {
-            $(this).prev('.control-label').addClass('active');
-        });
-        $('.form-control').focusout(function () {
-            $(this).prev('.control-label').removeClass('active');
-        });
-        $("#validForm").validate({
-            ignore: ":hidden",
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2,
-                    maxlength: 16,
-                },
-                email: {
-                    required: true,
-                    email: true,
-                },
-                subject: {
-                    required: true,
-                    minlength: 4,
-                    maxlength: 32,
-                },
-                message: {
-                    required: true,
-                    minlength: 16,
-                },
-            },
-            messages: {
-                name: {
-                    required: "<span>Please enter your name</span>",
-                    minlength: "<span>Your name must consist of at least 2 characters</span>",
-                    maxlength: "<span>The maximum number of characters - 24</span>",
-                },
-                email: {
-                    required: "<span>Please enter your email</span>",
-                    email: "<span>Please enter a valid email address.</span>",
-                },
-                subject: {
-                    required: "<span>Please enter your subject</span>",
-                    minlength: "<span>Your name must consist of at least 2 characters</span>",
-                    maxlength: "<span>The maximum number of characters - 16</span>",
-                },
-                message: {
-                    required: "<span>Please write me message</span>",
-                    minlength: "<span>Your message must consist of at least 16 characters</span>",
-                    maxlength: "<span>The maximum number of characters - 100 </span>",
-                },
-            },
-            submitHandler: function (form) {
-                $.ajax({
-                    type: "POST",
-                    url: "contact.php",
-                    data: $(form).serialize(),
-                    beforeSend: function () {
-                        // do something
-                    },
-                    success: function (data) {
-                        if (data == "Email sent!") ;
-                        $('input, textarea').val('');
-                        $('.form-group').blur();
-                        // do something
-                    }
-                });
-                return false;
-            }
-        });
-    }
-}
-
 // Typed
 var TxtRotate = function (el, toRotate, period) {
     this.toRotate = toRotate;
@@ -339,27 +223,18 @@ TxtRotate.prototype.tick = function () {
 
 window.onload = function () {
     var elements = document.getElementsByClassName('txt-rotate');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    setTimeout(function () {
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-rotate');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
         }
-    }
+    }, 2000);
 };
 
 new WOW().init();
-
-// header screenshots slider
-jQuery('.header-slider-tel').owlCarousel({
-    animateOut: 'fadeOut',
-    mouseDrag: false,
-    loop: true,
-    navText: [''],
-    items: 1,
-    autoplay: true,
-    smartSpeed: 450
-});
 
 //preloader
 $(window).on('load', function () {
@@ -387,30 +262,6 @@ function slider_slide_3() {
             },
             pagination: {
                 el: '.swiper-pagination-3',
-                clickable: true,
-            },
-        });
-    }
-}
-
-function slider_slide_6_tel() {
-    if ($.exists('.swiper-container-6-tel')) {
-        var swiper = new Swiper('.swiper-container-6-tel', {
-            initialSlide: 0,
-            pagination: {
-                el: '.swiper-pagination-6-tel',
-                clickable: true,
-            },
-        });
-    }
-}
-
-function slider_slide_9_tel() {
-    if ($.exists('.swiper-container-9-tel')) {
-        var swiper = new Swiper('.swiper-container-9-tel', {
-            initialSlide: 0,
-            pagination: {
-                el: '.swiper-pagination-9-tel',
                 clickable: true,
             },
         });
